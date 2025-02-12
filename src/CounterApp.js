cb8fe415e5155699932698593aaa134c59c3c9fa
Deepak Rockz <deepakrockz7@gmail.com>
2025-02-12 22:24:27 +0530

    Initial commit for GitHub Pages
    <<<<<<< HEAD
    Your current code
    =======
    The incoming code from rebase
    >>>>>>> branch-name

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import "./Counterapp.css"; 
 
@@ -15,3 +16,22 @@ function CounterApp() {
 }
 
 export default CounterApp;
+=======
+import React, { useState } from "react";
+import "./Counterapp.css"; 
+
+function CounterApp() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h2>Counter Value: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count > 0 ? count - 1 : 0)}>Decrement</button> 
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default CounterApp;
+>>>>>>> 0235ccb (Initial commit for GitHub Pages)
